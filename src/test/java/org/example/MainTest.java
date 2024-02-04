403e73f1ce8970576f1eb7ae8e6b2b49548ecb7c
Thomas van Otterloo <vanotterloo.thomas@gmail.com>
2024-02-04 18:05:22 +0100

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void testMain() {
+        // set default test
+        assertEquals(1, 1);
+    }
+
+}
